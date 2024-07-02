7b37b4ecdc2895498d8503a6d902de16a28c90ff
Pranto sheikh <prantosheikh834@gmail.com>
2024-07-02 19:13:40 +0600

    add some code

--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -8,9 +8,53 @@ import {
 	TOGGLED,
 } from "./actionTypes";
 
+const nextTodoId = (todos) => {
+	const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+	return maxId + 1;
+};
+
 const reducer = (state = initailState, action) => {
 	switch (action.type) {
 		case ADDED:
+			return [...state, { id: nextTodoId(state) }];
+
+		case TOGGLED:
+			return state.map((todo) => {
+				if (todo.id !== action.payload) {
+					return todo;
+				}
+
+				return {
+					...todo,
+					completed: !completed,
+				};
+			});
+
+		case COLORSELECTED:
+			const { todoId, color } = action.payload;
+
+			return state.map((todo) => {
+				if (todo.id !== todoId) {
+					return todo;
+				}
+
+				return {
+					...todo,
+					color: color,
+				};
+			});
+
+		case COLORSELECTED:
+			return state.filter((todo) => todo.id !== action.payload);
+
+		case ALLCOMPLETED:
+			return state.map((todo) => {
+				return {
+					...todo,
+					completed: true,
+				};
+			});
+			break;
 
 		default:
 			break;
