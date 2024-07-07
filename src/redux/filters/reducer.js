9d342db45cca88931d8a793a0717919cdbafec79
Pranto sheikh <prantosheikh834@gmail.com>
2024-07-07 19:44:25 +0600

    first commit

--- src/redux/filters/reducer.js
+++ src/redux/filters/reducer.js
@@ -1,47 +1,38 @@
-import { COLORCHANGED, STATUSCHANGED } from './actionTypes'
-import initialState from './initialState'
-
+import { COLORCHANGED, STATUSCHANGED } from "./actionTypes";
+import initialState from "./initialState";
 
 const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case STATUSCHANGED:
+			return {
+				...state,
+				status: action.payload,
+			};
+		case COLORCHANGED:
+			const { color, changeType } = action.payload;
 
-   switch (action.type) {
-      case STATUSCHANGED:
-         return {
-            ...state,
-            status: action.payload
-         }
-      case COLORCHANGED:
-         const { color, changeType } = action.payload;
+			switch (changeType) {
+				case "added":
+					return {
+						...state,
+						colors: [...state.colors, color],
+					};
 
-         switch (changeType) {
-            case 'added':
-               return {
-                  ...state,
-                  colors: [
-                     ...state.colors,
-                     color
-                  ]
-               }
+				case "remove":
+					return {
+						...state,
+						colors: state.colors.filter(
+							(existingColor) => existingColor !== color
+						),
+					};
 
-            case 'remove':
+				default:
+					return state;
+			}
 
-               return {
-                  ...state,
-                  colors: state.colors.filter((existingColor) => existingColor !== color)
-               }
+		default:
+			return state;
+	}
+};
 
-
-
-            default:
-               return state
-         }
-
-
-
-      default:
-         return state
-   }
-
-}
-
-export default reducer
+export default reducer;
