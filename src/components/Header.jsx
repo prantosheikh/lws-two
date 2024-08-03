0507e8e166fc29803ccaf19009c6ad5d3da6fa86
Pranto Sheikh <sheib-git>
2024-08-03 21:46:05 +0600

    one

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,48 +6,48 @@ import { added } from "../redux/todos/actions";
 import { useDispatch } from "react-redux";
 
 const Header = () => {
-	const dispatch = useDispatch();
-	const [input, setInput] = useState("");
-	console.log(input);
-	const handleInput = (e) => {
-		setInput(e.target.value);
-	};
+   const dispatch = useDispatch();
+   const [input, setInput] = useState("");
+   console.log(input);
+   const handleInput = (e) => {
+      setInput(e.target.value);
+   };
 
-	const submitHandle = (e) => {
-		e.eventpreventdefault();
-		dispatch(added(input));
-		setInput("");
-	};
+   const submitHandle = (e) => {
+      e.preventDefault();
+      dispatch(added(input));
+      setInput("");
+   };
 
-	return (
-		<div>
-			<form
-				className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
-				onSubmit={submitHandle}
-			>
-				<img src={notesImage} className="w-6 h-6" alt="Add todo" />
-				<input
-					type="text"
-					placeholder="Type your todo"
-					className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
-					value={input}
-					onChange={handleInput}
-				/>
-				<button
-					type="submit"
-					className={`appearance-none w-8 h-8 bg-[url(${plusImage})] bg-no-repeat bg-contain`}
-				></button>
-			</form>
+   return (
+      <div>
+         <form
+            className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+            onSubmit={submitHandle}
+         >
+            <img src={notesImage} className="w-6 h-6" alt="Add todo" />
+            <input
+               type="text"
+               placeholder="Type your todo"
+               className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+               value={input}
+               onChange={handleInput}
+            />
+            <button
+               type="submit"
+               className={`appearance-none w-8 h-8 bg-[url(${plusImage})] bg-no-repeat bg-contain`}
+            ></button>
+         </form>
 
-			<ul className="flex justify-between my-4 text-xs text-gray-500">
-				<li className="flex space-x-1 cursor-pointer">
-					<img className="w-4 h-4" src={tickImage} alt="Complete" />
-					<span>Complete All Tasks</span>
-				</li>
-				<li className="cursor-pointer">Clear completed</li>
-			</ul>
-		</div>
-	);
+         <ul className="flex justify-between my-4 text-xs text-gray-500">
+            <li className="flex space-x-1 cursor-pointer">
+               <img className="w-4 h-4" src={tickImage} alt="Complete" />
+               <span>Complete All Tasks</span>
+            </li>
+            <li className="cursor-pointer">Clear completed</li>
+         </ul>
+      </div>
+   );
 };
 
 export default Header;
